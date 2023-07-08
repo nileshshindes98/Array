52aba9ee792c2e1ea6b85bd14566e3ae65ebb1bf
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-08 10:33:20 +0530

    Added : Find Common String

--- CommonString.java
+++ CommonString.java
@@ -1,9 +1,42 @@
 package array;
+import java.util.Arrays;
+public class CommonString {
+    public static void main(String[] args) {
 
-public class CommonString
-{
-    String [] str1 ={"Nilesh","Yogesh","Rajat"};
-    String [] str2 ={"Rajat","Yogesh","Akshar"};
+        String[] array1 = {"Nilesh", "Yogesh", "Rajat"};
+        String[] array2 = {"Rajat", "Yogesh", "Akshar"};
 
+        int commonCount = 0;
 
+        for (String element1 : array1)
+        {
+            for (String element2 : array2)
+            {
+                if (element1.equals(element2))
+                {
+                    commonCount++;
+                    break;
+                }
+            }
+        }
+
+        String[] commonElements = new String[commonCount];
+        int index = 0;
+
+        for (String element1 : array1) {
+            for (String element2 : array2) {
+                if (element1.equals(element2)) {
+                    commonElements[index] = element1;
+                    index++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Common Element : ");
+        for (String commonElement : commonElements) {
+            System.out.println(commonElement);
+        }
+    }
 }
+
